b8528a25a9f1f079e3b40bccaee0196cd457bbc1
Christian Romeni <c.romeni@brainfactory.de>
2015-07-29 01:13:34 +0200

    rewrite to use FilesModel and system functions

--- classes/cLogRotateBackEnd.php
+++ classes/cLogRotateBackEnd.php
@@ -22,37 +22,22 @@ class cLogRotateBackEnd extends \BackendModule
 	{
         $this->import('Input');
         $realFolder = TL_ROOT . '/' . $GLOBALS['TL_CONFIG']['uploadPath'] . '/cLogRotate';
-        $yearFolders = scan($realFolder);
 
-        foreach ($yearFolders as $yearFolder) {
-            $logFiles = scan($realFolder . '/' . $yearFolder);
-            foreach ($logFiles as $logfile) {
-                $logfileabsolute = $realFolder . '/' . $yearFolder . '/' . $logfile;
-                $logfilerelative = $GLOBALS['TL_CONFIG']['uploadPath'] . '/cLogRotate/' . $yearFolder . '/' . $logfile;
-                $logname = str_replace('.log', '', $logfile);
-                $logfilename = date_format(date_create_from_format('Y-m-d_-_H-i-s', $logname), '[d.m.Y H:i]');
-                $yearLogs[$yearFolder][] = array('name' => $logfilename,
-                                                         'path' => $logfilerelative,
-                                                         'size' => $this->human_filesize(filesize($logfileabsolute))
-                                                         );
-            }
+        $subfolders = \FilesModel::findMultipleFoldersByFolder($GLOBALS['TL_CONFIG']['uploadPath'] . '/cLogRotate');
+
+        foreach ($subfolders as $key => $subfolder) {
+            $logFiles[$subfolder->name] = \FilesModel::findMultipleFilesByFolder($subfolder->path);
         }
         
         $this->Template->headline     = 'LogRotate Downloads';
         $this->Template->downloadText = $GLOBALS['TL_LANG']['cLogRotate']['download'];
-        $this->Template->logs         = $yearLogs;
+        $this->Template->logs         = $logFiles;
 
         if ($this->Input->get('dl')) {
-            $this->sendFileToBrowser($this->Input->get('dl'));
+            $file = \FilesModel::findById($this->Input->get('dl'));
+            $this->sendFileToBrowser($file->path);
             exit;
         }
 
 	}
-
-    protected function human_filesize($bytes, $dec = 2)
-    {
-        $size   = array('B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
-        $factor = floor((strlen($bytes) - 1) / 3);
-        return sprintf("%.{$dec}f", $bytes / pow(1024, $factor)) . @$size[$factor];
-    }
 }
